405a007dbbd4849199c235c14026c5ec81835a72
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-02 21:08:18 +0330

    Add files via upload

---
+++ WindowsFormsApp58/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp58
+{
+    public class cEmployee
+    {
+        public string firstname;
+        public string lastname;
+        public string identity;
+
+        public cEmployee()
+        {
+            firstname = "ali";
+            lastname = "yahyavi";
+        }
+
+        public cEmployee(string s1, string s2)
+        {
+
+        }
+
+        public string Concatinate()
+        {
+            return firstname + ' ' + lastname;
+        }
+
+        public string Concatinate(string s)
+        {
+            return firstname + ' ' + lastname + ' ' + s;
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp58/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp58
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cEmployee s1 = new cEmployee();
+            cEmployee s2 = new cEmployee();
+
+            s1.firstname = "gholi";
+            s2.lastname = "goli";
+
+            s2 = s1;
+
+            s1.firstname = "sanaz";
+            label1.Text = s2.Concatinate("hghghvhv");
+        }
+    }
+}
